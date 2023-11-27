fcb26ef96a2502c6e278e8b1ec781d80bb99da77
Dezedene <gregoire.suray@gmail.com>
2023-11-27 19:07:03 +0100

    event handler with useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+import Modal from './Modal'
+import Backdrop from './Backdrop'
+
+
 function Todo(props) {
+  const [modalIsOPen, setModalIsOpen] = useState(false)
+
+  function deleteHandler () {
+    setModalIsOpen(true)
+  }
+
   return(
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button onClick={deleteHandler} className="btn">Delete</button>
       </div>
+      { modalIsOPen ? <Modal /> : null }
+      { modalIsOPen ? <Backdrop /> : null }
     </div>
   )
 }
