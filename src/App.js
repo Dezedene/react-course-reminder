6247bc72ce2e86e8766ac3bc5c5efcbefdee8724
Dezedene <gregoire.suray@gmail.com>
2023-11-29 14:01:17 +0100

    cleaned project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import Todo from './components/Todo'
 
 function App() {
   return (
-    <>
-      <h1>My todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Master React"/>
-      <Todo text="Easy React"/>
-    </>
+      <h1>App</h1>
   );
 }
 
