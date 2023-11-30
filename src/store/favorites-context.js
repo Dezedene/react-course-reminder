c774a12f3ea3e2c9ed569879acb7a219ef53ec5f
Dezedene <gregoire.suray@gmail.com>
2023-11-30 16:14:23 +0100

    function to edit context ok

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -4,17 +4,49 @@ import { createContext, useState } from 'react'
 // Here are global values
 const FavoritesContext = createContext({
     favorites: [],
-    totalFavorites: 0
+    totalFavorites: 0,
+    addFavorites: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {},
 });
 
 // The component is here to update/ add /removed context values as its still a regular component
 // so we can still add him a regular state
-const FavoritesContextProvider = (props) => {
+export function FavoritesContextProvider (props) {
     const [ userFavorites , setUserFavorites ] = useState([]);
+
+    // when working with useState, the update is schedule and the update is not done instantly
+    // sometimes the state snapshot does not really reflect the real latest version as it is
+    // maybe not updated yet, and the update of our state depend of this latest state snapshot
+
+    // if we depend of the previous version of a state, there is an alternative way of updating the state 
+    // than just using setUserFavorites here for example, instead of passing the new value: setState(value),
+    // we sould pass a function to the state updating function:
+    // this way we are sure that react will execute things in the correct order, and we sure that
+    // we are working on the latest version of the state snapshot
+
+    function addFavoriteHandler(favoriteMeetup) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup)
+        })
+    }
+
+    function removeFavoriteHandler(meetupIp) {
+        setUserFavorites(prevUserFavorites => {
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupIp)
+        })
+    }
+
+    function itemIsFavoriteHandler(meetupId) {
+        return userFavorites.some(meetup => meetup.id === meetupId)
+    }
 
     const context = {
         favorites:userFavorites,
         totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler
     };
 
     // wrapping 'props.children' in FavoritesContext.Provider allow us to wrap any component 
@@ -26,5 +58,5 @@ const FavoritesContextProvider = (props) => {
   )
 }
 
-export default FavoritesContextProvider
 
+export default FavoritesContext
