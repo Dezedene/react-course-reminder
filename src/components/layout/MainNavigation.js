1af2408f899f3c49f695a4690e2f725a189321c9
Dezedene <gregoire.suray@gmail.com>
2023-12-04 13:19:14 +0100

    added a counter badge on navigation bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,33 @@
+import { useContext } from 'react'
+
 import { Link } from 'react-router-dom'
 
 import mainNavigationStyle from './MainNavigation.module.css'
+import FavoritesContext from './../../store/favorites-context'
 
 const MainNavigation = () => {
+    const favoritesCtx = useContext(FavoritesContext);
+
   return (
-      <header className={ mainNavigationStyle.headerStyle }>
-        <div className={ mainNavigationStyle.logo }>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetups</Link>
-                </li>
-                <li>
-                    <Link to='/new-meetup'>New Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>Favorites</Link>
-                </li>
-            </ul>
-        </nav>
-      </header>
+    <header className={ mainNavigationStyle.headerStyle }>
+      <div className={ mainNavigationStyle.logo }>React Meetups</div>
+      <nav>
+          <ul>
+            <li>
+              <Link to='/'>All Meetups</Link>
+            </li>
+            <li>
+              <Link to='/new-meetup'>New Meetup</Link>
+            </li>
+            <li>
+              <Link to='/favorites'>
+                  Favorites
+                  <span className={ mainNavigationStyle.badge }>{ favoritesCtx.totalFavorites }</span>
+              </Link>
+            </li>
+          </ul>
+      </nav>
+    </header>
   )
 }
 
